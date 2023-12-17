d2d663f75dcfa325bdd9dce2650363cbdb30a953
Emils Valdmanis <valdmanis71@gmail.com>
2023-12-17 22:09:24 +0200

    Added Firebase

---
+++ src/utils/firebase.utils.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app"
+import { getAnalytics } from "firebase/analytics"
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA-otJS4iYpvLrYnrl35kN0ggFZIQT99Do",
+  authDomain: "lunaportfolio-790ba.firebaseapp.com",
+  projectId: "lunaportfolio-790ba",
+  storageBucket: "lunaportfolio-790ba.appspot.com",
+  messagingSenderId: "475127802794",
+  appId: "1:475127802794:web:a5e295abe8ff9f953619d3",
+  measurementId: "G-MVBB2XLN9L"
+}
+
+
+const app = initializeApp(firebaseConfig)
+const analytics = getAnalytics(app)
