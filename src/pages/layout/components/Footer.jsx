9d25c1a1877d4a51a5f0eb8351088eadc5aa18fe
Emils Valdmanis <valdmanis71@gmail.com>
2023-12-30 22:20:30 +0200

    Dont show link to login if logged in

--- src/pages/layout/components/Footer.jsx
+++ src/pages/layout/components/Footer.jsx
@@ -1,10 +1,10 @@
 import React from "react"
 import { Link } from "react-router-dom"
 
-const Footer = () => {
+const Footer = ({ user }) => {
   return (
     <div className="flex p-10 w-full bg-gray-100 font-calligraphy text-3xl justify-between">
-      <Link to={'/admin/login'}>Login to upload an image</Link>
+      {!user && <Link to={'/admin/login'}>Login to upload an image</Link>}
       <p><span className="text-xl">©</span> Luna. All rights reserved</p>
     </div>
   )
--- src/pages/layout/index.jsx
+++ src/pages/layout/index.jsx
@@ -21,7 +21,9 @@ const Layout = () => {
           <Outlet/>
         </Suspense>
       </main>
-      <Footer/>
+      <Footer
+        user={user}
+      />
     </div>
   )
 }
