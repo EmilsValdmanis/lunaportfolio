ae2bc229e48d20c1d9c825ddabb3946884fb3636
Emils Valdmanis <valdmanis71@gmail.com>
2024-01-22 20:14:18 +0200

    Fixed the filter

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -58,13 +58,14 @@ const Home = () => {
       ) : (
         <div className="flex flex-col gap-8">
           {Object.keys(allImages)
-            .sort((a, b) => new Date(b) - new Date(a))
             .map((month, index) => (
               <div key={index}>
                 <div className="max-w-7xl m-auto flex flex-col gap-6">
                   <h2 className="text-4xl font-calligraphy">{formatMonthTitle(month)}</h2>
                   <div className="grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-4 gap-4">
-                    {allImages[month].map((image, index) => (
+                    {allImages[month]
+                      .sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt))
+                      .map((image, index) => (
                       <div key={index} className="rounded-xl overflow-hidden shadow hover:shadow-xl transition-transform transform duration-300 hover:scale-105">
                         <img
                           src={image.url}
