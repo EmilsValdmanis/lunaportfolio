c9147af50bd0cec289dbb79f6fdb6dc5caaeb890
Emils Valdmanis <valdmanis71@gmail.com>
2024-08-08 23:00:16 +0300

    Added navbar link animations

--- src/pages/layout/components/Navbar.jsx
+++ src/pages/layout/components/Navbar.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { auth } from "../../../utils/firebase.utils";
 import { signOut } from "firebase/auth";
+import { motion } from "framer-motion";
 
 const Navbar = ({ user }) => {
     const navigate = useNavigate();
@@ -12,42 +13,100 @@ const Navbar = ({ user }) => {
         navigate("/");
     };
 
+    const linkVariants = {
+        initial: {
+            y: -100,
+        },
+        animate: {
+            y: 0,
+            transition: {
+                type: "ease",
+                stiffness: 100,
+                damping: 10,
+            },
+        },
+        hover: {
+            scale: 1.1,
+            rotate: [0, 5, -5, 5, 0],
+            transition: {
+                type: "spring",
+                stiffness: 300,
+                damping: 10,
+            },
+        },
+    };
+
     return (
         <div className="supports-[backdrop-filter]:bg-background/60 top-0 z-50 w-full bg-gray-100/80 backdrop-blur md:sticky">
             <div className="flex w-full flex-col items-center justify-between gap-2 px-16 py-8 font-calligraphy md:flex-row">
-                <Link
-                    to={`/`}
-                    className="border-b-2 border-transparent text-4xl hover:border-orange-500"
+                <motion.div
+                    variants={linkVariants}
+                    initial="initial"
+                    animate="animate"
+                    whileHover="hover"
                 >
-                    Luna
-                </Link>
-                <div className="flex gap-8 text-2xl md:text-3xl">
                     <Link
                         to={`/`}
-                        className="border-b-2 border-transparent hover:border-orange-500"
+                        className="border-b-2 border-transparent text-4xl hover:border-orange-500"
                     >
-                        Home
+                        Luna
                     </Link>
-                    <Link
-                        to={`/about`}
-                        className="border-b-2 border-transparent hover:border-orange-500"
+                </motion.div>
+                <div className="flex gap-8 text-2xl md:text-3xl">
+                    <motion.div
+                        variants={linkVariants}
+                        initial="initial"
+                        animate="animate"
+                        whileHover="hover"
                     >
-                        About
-                    </Link>
+                        <Link
+                            to={`/`}
+                            className="border-b-2 border-transparent hover:border-orange-500"
+                        >
+                            Home
+                        </Link>
+                    </motion.div>
+                    <motion.div
+                        variants={linkVariants}
+                        initial="initial"
+                        animate="animate"
+                        whileHover="hover"
+                    >
+                        <Link
+                            to={`/about`}
+                            className="border-b-2 border-transparent hover:border-orange-500"
+                        >
+                            About
+                        </Link>
+                    </motion.div>
                     {user && (
                         <>
-                            <Link
-                                to={"/admin/upload"}
-                                className="border-b-2 border-transparent hover:border-orange-500"
+                            <motion.div
+                                variants={linkVariants}
+                                initial="initial"
+                                animate="animate"
+                                whileHover="hover"
                             >
-                                Upload
-                            </Link>
-                            <button
-                                onClick={logoutUser}
-                                className="border-b-2 border-transparent hover:border-orange-500"
+                                <Link
+                                    to={"/admin/upload"}
+                                    className="border-b-2 border-transparent hover:border-orange-500"
+                                >
+                                    Upload
+                                </Link>
+                            </motion.div>
+                            <motion.div
+                                variants={linkVariants}
+                                initial="initial"
+                                animate="animate"
+                                whileHover="hover"
                             >
-                                Log out
-                            </button>
+                                <button
+                                    onClick={logoutUser}
+                                    className="border-b-2 border-transparent hover:border-orange-500"
+                                >
+                                    Log out
+                                </button>
+                            </motion.div>
                         </>
                     )}
                 </div>
